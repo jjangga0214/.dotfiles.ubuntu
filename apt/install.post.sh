748a1e2f9b3f2ba8a1f8b67e99a041af7a19e59c
GIL B. Chan <bnbcmindnpass@gmail.com>
2019-04-10 11:51:01 +0900

    update ddc

--- apt/install.post.sh
+++ apt/install.post.sh
@@ -1,6 +1,8 @@
 BASE_DIR=$(dirname "$0")
 
 bash $BASE_DIR/docker.sh
-add-apt-repository -y ppa:micahflee/ppa
+add-apt-repository -y ppa:micahflee/ppa # torbrowser-launcher
+add-apt-repository -y ppa:peek-developers/stable # peek
 apt update
 apt install -y torbrowser-launcher
+apt install -y peek
