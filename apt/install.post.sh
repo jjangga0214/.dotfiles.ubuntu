7d70c48909a53a5f3a4a7f460429a31b91831782
GIL B. Chan <bnbcmindnpass@gmail.com>
2020-11-11 23:16:03 +0900

    feat: comment unessential installations

--- apt/install.post.sh
+++ apt/install.post.sh
@@ -1,8 +1,8 @@
 BASE_DIR=$(dirname "$0")
 
 bash $BASE_DIR/docker.sh
-add-apt-repository -y ppa:micahflee/ppa # torbrowser-launcher
-add-apt-repository -y ppa:peek-developers/stable # peek
+# add-apt-repository -y ppa:micahflee/ppa # torbrowser-launcher
+# add-apt-repository -y ppa:peek-developers/stable # peek
 apt update
-apt install -y torbrowser-launcher
-apt install -y peek
+# apt install -y torbrowser-launcher
+# apt install -y peek
